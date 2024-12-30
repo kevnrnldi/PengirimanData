b5f059b6d567409da21fe339e5c348787f1d4299
mkevinrinaldi <mkevinrinaldi@gmail.com>
2024-12-30 21:42:54 +0700

    menambahkan beberapa syntax

--- src/index.kt
+++ src/index.kt
@@ -1,5 +1,16 @@
 fun main() {
-    fun kevin(){
 
-    }
+    var age: Byte = 6_4
+    var angka: Long = 1_000_000_000_000_0L
+    var angkaBaru: Int = angka.toInt()
+    println(angkaBaru)
+    var umur: Double = 2.30
+  //  println( age + umur - angka)
+
+    var binary: Int = 0b10101110
+    println(binary)
+
+    var char1: Char = 'e'
+    var benar: Boolean = true
+    var salah: Boolean = false
 }
