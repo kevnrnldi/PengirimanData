0ec0a82bdd5571611653c5e69c91b1f4193731e1
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-01-01 18:16:03 +0700

    commit ketiga saya belajar tentang const val,val,var / mutable dan immutable serta belajar tentang margin untuk teks

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,35 @@
 fun main() {
 
-    var nama = "kevin";
-    println("Hello $nama!")
+   var firstName: String = "Kevin"
+   var LastName: String = "VIN"
+    println(firstName + " " + LastName)
 
+
+
+
+    var text: String = """
+        Nama saya kevin
+        saya tinggal di singapura
+        mobil saya porsche
+    """
+
+
+
+    println("saya ingin porsche dan ${text}")
+    println("total char ${firstName + " " + LastName} = ${firstName.length + LastName.length}")
+
+//immutable/konsisten variable val dan const
+    //const khusus untuk global atau digunakna dalam OOP
+        //mutable variable var
+    //nullable
+    val nilai1 = 12
+    var nilai2 = 13
+    var nilai3: String? = null
+    println("${nilai3} ${nilai2 + nilai1}")
 
 
 
 }
+
+//hanya bisa digunakan diluar local function/class
+const val APP = "application"
