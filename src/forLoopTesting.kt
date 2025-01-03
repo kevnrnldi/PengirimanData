de032268f2df9f0e6d9af06a4525d91039564d88
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-01-03 11:37:41 +0700

    belajar looping

---
+++ src/forLoopTesting.kt
@@ -0,0 +1,48 @@
+fun main(){
+    val nilai = arrayOf(89,76,88,56,90,76,56,90)
+    val mobilKu = arrayOf("porsche","mclaren","mercedes benz","ferrari")
+
+    var i = 0
+    do{
+    println("indeks ke $i")
+        i--
+    }while (i > -10)
+
+
+
+    /*
+
+    while (i < 10){
+        println("index ke ${i}")
+        i++
+    }
+*/
+
+ /*   var cekMobilku = mobilKu.size - 1
+    for (i in 0.. cekMobilku){
+        println("index ke ${i} = ${mobilKu.get(i)}")
+    }
+*/
+    //for loop
+ /*   var jumlahPerulangan = 0
+    for(name in nilai){
+     when{
+         name >= 90 -> println("Nice Job")
+         name >= 80 -> println("Good")
+         name >= 70 -> println("Not Bad")
+         else -> println("Try Again Next Year")
+
+     }
+        jumlahPerulangan++
+    }
+    println("Total jumlah perulangan = ${jumlahPerulangan}")
+}*/
+
+//range
+/*
+    for(nilais in 0..100 step 6){
+    println(nilais)
+    }
+    */
+
+}
