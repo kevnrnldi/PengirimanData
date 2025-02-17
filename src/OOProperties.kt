2ffdf6fd63e4115249c7a1b7f02cb75fbe7906d7
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-02-17 21:59:53 +0700

    beljaar function

---
+++ src/OOProperties.kt
@@ -0,0 +1,42 @@
+
+fun main() {
+    val fullName = Person("muhammad","kevin","rinaldi")
+    fullName.nama()
+    val objectName = orang()
+    objectName.firstName
+
+
+    val objectCar = orang()
+    objectCar.middleName = "  porsche"
+
+    val objectMoney = orang()
+    objectMoney.lastName ="  rich"
+
+    println(objectName.firstName)
+    println(objectMoney.lastName)
+    println(objectCar.middleName)
+}
+
+class orang()
+{
+
+    val firstName: String = "apala"
+    var middleName: String? = null
+    var lastName: String = ""
+
+
+}
+
+
+
+
+class Person(
+    var firstName: String = "",
+    var middleName: String? = null,
+    val lastName: String = ""
+) {
+    fun nama (){
+       println("nama lengkap saya $firstName $middleName $lastName")
+    }
+
+}
