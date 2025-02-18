33ce0f52fe29402440a0d616ce05e1541db22aac
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-02-18 20:57:28 +0700

    belajar secondary constructor

--- src/app/newData.kt
+++ src/app/newData.kt
@@ -1,30 +1,61 @@
 package app
 
+import data.Business
 import data.Mobil
 
 fun main (){
-    val cayman = Mobil("porsche", 2024)
-    cayman.yearCar = 2026
+//    val cayman = Mobil("porsche", 2024)
+//    cayman.yearCar = 2026
+//
+//    val gtr = Mobil("nissan", 2021)
+//
+//
+//
+//
+//    println(cayman.brandCar)
+//    println(cayman.yearCar)
+//    println(gtr.brandCar)
+//    println(gtr.yearCar)
 
-    val gtr = Mobil("nissan", 2021)
 
 
 
-    println(cayman.brandCar)
-    println(cayman.yearCar)
-    println(gtr.brandCar)
-    println(gtr.yearCar)
+    val game2 = Game("God Of War")
+    game2.tahunku = 2022
+    game2.tema = "Action"
+   val game1 = Game("Elden ring", "Soulslike")
+   val game3 = Game("forza horizon 6", "motorsport", 2026)
 
 
 
-    val game1 = game("Elden ring", "Soulslike")
-  
+
+    var bisnis1 = Business("AutoSport", "Otomotif")
+    var bisnis2 = Business("Breads", "Makanan", 2025)
+
+    println(bisnis1.namaBisnis)
+    println(bisnis2.namaBisnis)
+
+    bisnis1.tampil()
+    bisnis2.tampil()
 
 }
 
 
-class game(var nama: String, var genre: String){
+class Game( nama: String,  genre: String,  tahun: Int){
     init {
-        println("saya suka bermain game $genre contohnya seperti $nama")
+        println("saya suka bermain game $nama rilis tahun $tahun dan bergenre $genre")
     }
+
+    constructor(nama: String,genre: String) : this(nama,genre,2025){
+        println("secondary constructor pertama")
+    }
+
+    constructor(nama: String) : this(nama,""){
+        println("secondary constructor kedua")
+    }
+
+
+    var tahunku: Int = tahun
+    var tema : String = genre
+
 }
--- src/data/Business.kt
+++ src/data/Business.kt
@@ -1,4 +1,27 @@
 package data
 
 class Business {
+    var namaBisnis: String
+    var temaBisnis: String
+    var tahunBerdiri: Int? = null
+
+    constructor(paramNama: String, paramTema: String){
+         namaBisnis = paramNama
+         temaBisnis = paramTema
+
+    }
+    constructor(paramNama: String,paramTema: String,paramTahun: Int) : this(paramNama,paramTema){
+         tahunBerdiri = paramTahun
+
+    }
+
+
+    fun tampil(){
+        if(tahunBerdiri != null){
+            println("saya punya bisnis $namaBisnis dan tema bisnis saya yaitu $temaBisnis berdiri sejak $tahunBerdiri")
+        } else {
+            println("saya punya bisnis $namaBisnis dan tema bisnis saya yaitu $temaBisnis")
+        }
+    }
+
 }
