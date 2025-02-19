4a040569c1d5f198fddad7159bb874de87e73e81
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-02-19 21:44:11 +0700

    belajar function

--- src/app/newData.kt
+++ src/app/newData.kt
@@ -4,58 +4,58 @@ import data.Business
 import data.Mobil
 
 fun main (){
-//    val cayman = Mobil("porsche", 2024)
-//    cayman.yearCar = 2026
+    val cayman = Mobil("porsche", 2024)
+    cayman.year = 2026
 //
 //    val gtr = Mobil("nissan", 2021)
-//
-//
-//
-//
-//    println(cayman.brandCar)
-//    println(cayman.yearCar)
+
+
+
+
+    println(cayman.brand)
+    println(cayman.year)
 //    println(gtr.brandCar)
 //    println(gtr.yearCar)
 
 
 
 
-    val game2 = Game("God Of War")
-    game2.tahunku = 2022
-    game2.tema = "Action"
-   val game1 = Game("Elden ring", "Soulslike")
-   val game3 = Game("forza horizon 6", "motorsport", 2026)
+//    val game2 = Game("God Of War")
+//    game2.tahunku = 2022
+//    game2.tema = "Action"
+//   val game1 = Game("Elden ring", "Soulslike")
+//   val game3 = Game("forza horizon 6", "motorsport", 2026)
 
 
 
 
-    var bisnis1 = Business("AutoSport", "Otomotif")
-    var bisnis2 = Business("Breads", "Makanan", 2025)
 
-    println(bisnis1.namaBisnis)
-    println(bisnis2.namaBisnis)
-
-    bisnis1.tampil()
-    bisnis2.tampil()
-
-}
-
-
-class Game( nama: String,  genre: String,  tahun: Int){
-    init {
-        println("saya suka bermain game $nama rilis tahun $tahun dan bergenre $genre")
-    }
-
-    constructor(nama: String,genre: String) : this(nama,genre,2025){
-        println("secondary constructor pertama")
-    }
-
-    constructor(nama: String) : this(nama,""){
-        println("secondary constructor kedua")
-    }
-
-
-    var tahunku: Int = tahun
-    var tema : String = genre
+//    var bisnis1 = Business("AutoSport", "Otomotif")
+//    var bisnis2 = Business("Breads", "Makanan", 2025)
+//
+//    println(bisnis1.namaBisnis)
+//    println(bisnis2.namaBisnis)
+//
+//    bisnis1.tampil()
+//    bisnis2.tampil()
 
 }
+
+//class Game( nama: String,  genre: String,  tahun: Int){
+//    init {
+//        println("saya suka bermain game $nama rilis tahun $tahun dan bergenre $genre")
+//    }
+//
+//    constructor(nama: String,genre: String) : this(nama,genre,2025){
+//        println("secondary constructor pertama")
+//    }
+//
+//    constructor(nama: String) : this(nama,""){
+//        println("secondary constructor kedua")
+//    }
+//
+//
+//    var tahunku: Int = tahun
+//    var tema : String = genre
+//
+//}
--- src/data/Mobil.kt
+++ src/data/Mobil.kt
@@ -2,7 +2,35 @@ package data
 
 
 
-class Mobil (brand: String? = "Porsche",year: Int){
-    var brandCar: String? = brand
-    var yearCar: Int = year
+class Mobil (var brand: String? = "Porsche",var year: Int){
+//    var brandCar: String? = brand
+//    var yearCar: Int = year
+}
+
+
+
+fun main() {
+
+    val saya = kevin()
+    saya.mobil = "porsche"
+    saya.name = "kevin"
+    saya.jumlahMobil = 12
+
+    saya.sayHello("yuna")
+    var fulling = saya.full()
+    println(fulling)
+
+
+}
+
+class kevin(var name: String = "",var mobil: String = "",var jumlahMobil : Int? = 5 ){
+
+    fun sayHello(namaDia: String){
+        println("Hallo $namaDia, saya $name")
+    }
+
+    fun full() : String{
+        return "saya $name punya mobil $mobil dan jumlah mobil saya $jumlahMobil"
+    }
+
 }
