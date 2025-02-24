9847feb2899895b39439e5d9ccefddcb1b980dbf
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-02-24 20:23:08 +0700

    belajar

--- src/infinite.kt
+++ src/infinite.kt
@@ -8,8 +8,24 @@ fun main() {
 
     val test = lambdaCoba()
     test.jalankan()
+
+
+    var nama = anyTesting("kevin")
+    var umur = anyKedua(20)
+    println(nama.toString())
+    println(umur.toString())
 }
 
+class anyTesting(var nama: String){
+    override fun toString(): String {
+        return "my name $nama"
+    }
+}
+class anyKedua(var umur: Int){
+    override fun toString(): String {
+        return "i'm $umur years old"
+    }
+}
 
 class lambdaCoba{
 
