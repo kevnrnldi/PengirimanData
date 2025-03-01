a90373c841b894a436920d3faef0f7673916c6e7
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-03-01 22:04:41 +0700

    belajar equals string

--- src/checking.kt
+++ src/checking.kt
@@ -2,12 +2,20 @@ fun main(){
     PanggilDenganWhen(laptop("axioo"))
 
 
-    percobaan("porsche dan bmw z4")
-    percobaan(911)
+//    percobaan("porsche dan bmw z4")
+//    percobaan(911)
 
 
-    val check = kitaTOString("ya")
-    println(check)
+//    val check = kitaTOString("ya")
+//    println(check)
+
+
+    var percobaan1 = bandingkan("razer")
+    var percobaan2 = bandingkan("raZer")
+
+    println(percobaan1.equals(percobaan2))
+    println(percobaan1 == percobaan2)
+    println(percobaan1 == percobaan2)
 }
 
 fun Panggil(any: Any){
@@ -46,6 +54,15 @@ fun percobaan(mobil: Any){
 class kitaTOString(var apakahBerhasil: String){
     override fun toString(): String {
         return "berhasil? $apakahBerhasil"
+    }
+}
+
+class bandingkan(val perusahaan: String){
+    override fun equals(other: Any?): Boolean {
+        return when(other){
+            is bandingkan -> other.perusahaan.equals( this.perusahaan, ignoreCase = true)
+            else -> false
+        }
     }
 }
 
