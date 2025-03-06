858324b59965264512392bc90065828a9fc04b6a
mkevinrinaldi <mkevinrinaldi@gmail.com>
2025-03-06 16:54:37 +0700

    belajar lateinit

--- src/app/panggilkan.kt
+++ src/app/panggilkan.kt
@@ -1,5 +1,7 @@
 package app
 
+import com.company.belajar.util.sayhello
+import data.Mobil
 import tempatTinggal
 
 
@@ -16,6 +18,15 @@ fun main(){
     coba2.fast("Burger King")
     println(coba2.burger)
 
+
+//    var person = memanggilInterface("kevin")
+//    person.sayHello("apad")
+    var person2 = memanggilInterface("joko","ferrari")
+    person2.kendaraan("celyn","ferrari")
+
+    var human = multipleInterface("entah")
+    human.belok()
+
 }
 
 
@@ -28,4 +39,57 @@ class fastfood{
     }
 }
 
+
+interface Interaction {
+    var nama: String
+    fun sayHello(nama: String)  {
+        println(" nama saya ${this.nama} dan nama teman saya $nama")
+    }
+
+}
+
+interface Berjalan : Interaction {
+    var mobil: String
+    fun jalan(nama: String){
+       println("${this.nama} : mantap jiwa $nama")
+    }
+    fun kendaraan(nama: String, mobil: String){
+       println("${this.nama} mobilmu $mobil lebih kencang dari punya $nama")
+    }
+}
+
+interface belokKanan {
+    var belok: String
+    fun belok(){
+        println("belok kanan")
+    }
+}
+
+interface belokKiri  {
+    var belok: String
+    fun belok(){
+        println("belok kiri")
+    }
+}
+
+class multipleInterface(override var belok: String) : belokKiri, belokKanan {
+    override fun belok() {
+        if (belok == "kiri") {
+            super<belokKiri>.belok()
+        } else if(belok == "kanan") {
+            super<belokKanan>.belok()
+        } else {
+            println("input salah")
+        }
+    }
+
+
+}
+
+class memanggilInterface(override var nama: String, override var mobil: String) :  Berjalan {
+//    override fun sayHello(nama: String) {
+//        println(" nama saya ${this.nama} dan nama teman saya $nama")
+//    }
+
+}
 
